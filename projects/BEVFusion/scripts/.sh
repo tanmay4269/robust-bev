694d53ae75f4c2cd1ec2256b31758aa36863ae99
Tanmay Gejapati <tanmay4269@gmail.com>
2024-12-21 05:16:15 +0000

    improved run script

--- projects/BEVFusion/scripts/.sh
+++ projects/BEVFusion/scripts/.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-GPUS=1
+MODE=testing
 
-cd /tmp/mmdetection_dev/mmdetection3d
+WORKSPACE_PATH=/workspace
+CONFIG_PATH=projects/BEVFusion/configs/bevfusion_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
+# CKPT_PATH=/workspace/projects/BEVFusion/checkpoints/bevfusion-trained.pth
+CKPT_PATH=/workspace/projects/BEVFusion/checkpoints/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d-5239b1af.pth
+TRAINING_BATCH_SIZE=1
+TESTING_BATCH_SIZE=6
 
-# --- Lidar-only training ---
-# bash tools/dist_train.sh \
-#     projects/BEVFusion/configs/bevfusion_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py\
-#     $GPUS
-
-# --- Full training ---
-CKPT1=/tmp/mmdetection_dev/mmdetection3d/projects/BEVFusion/checkpoints/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d-5239b1af.pth
-CKPT2=/workspace/projects/BEVFusion/checkpoints/bevfusion_converted.pth
-CKPT3=/workspace/projects/BEVFusion/checkpoints/bevfusion_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d-2628f933.pth
-CKPT4=/tmp/mmdetection_dev/mmdetection3d/projects/BEVFusion/checkpoints/bevfusion-trained.pth
-
-# LIDAR_PRETRAINED_CHECKPOINT=/workspace/projects/BEVFusion/checkpoints/bevfusion_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d-2628f933.pth
-# IMAGE_PRETRAINED_BACKBONE=/workspace/projects/BEVFusion/checkpoints/swint-nuimages-pretrained.pth
-# bash tools/dist_train.sh \
-#     projects/BEVFusion/configs/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py \
-#     $GPUS \
-#     --cfg-options \
-#     load_from=${LIDAR_PRETRAINED_CHECKPOINT} \
-#     model.img_backbone.init_cfg.checkpoint=${IMAGE_PRETRAINED_BACKBONE}
-
-PYTHONPATH="tools/..":$PYTHONPATH \
-python tools/train.py \
-    projects/BEVFusion/configs/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py \
-    --cfg-options load_from=$CKPT4
-
-# NOTES: 
-# - CKPT1 has size mismatch for pts_middle_encoder.* => unusable
-# - CKPT2 needs manual change of vtransform to view_transform in the state_dict
+cd $WORKSPACE_PATH
+export PYTHONPATH=$PYTHONPATH:$WORKSPACE_PATH
+if [ $MODE == "training" ]; then
+    python tools/train.py \
+        $CONFIG_PATH \
+        --cfg-options \
+            load_from=$CKPT_PATH \
+            train_dataloader.batch_size=$TRAINING_BATCH_SIZE
+elif [ $MODE == "testing" ]; then
+    python tools/test.py \
+        $CONFIG_PATH \
+        $CKPT_PATH \
+        --cfg-options \
+            test_dataloader.batch_size=$TESTING_BATCH_SIZE
+else
+    echo "Invalid mode, choose between 'training' or 'testing'"
+fi
