cc6d5f82f9d2570f1614ff09dfe6662f90e01762
Tanmay Gejapati <tanmay4269@gmail.com>
2024-12-25 09:25:00 +0530

    modified testing scripts for expts

--- projects/BEVFusion/scripts/.sh
+++ projects/BEVFusion/scripts/.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-MODE=training
+MODE=pretraining
 
 WORKSPACE_PATH=/workspace
-CONFIG_PATH=projects/BEVFusion/configs/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
-CKPT_PATH=/workspace/projects/BEVFusion/checkpoints/bevfusion-trained.pth
-# CKPT_PATH=/workspace/projects/BEVFusion/checkpoints/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d-5239b1af.pth
+
+LIDAR_ONLY_CONFIG_PATH=projects/BEVFusion/configs/bevfusion_lidar_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
+LIDAR_CAM_CONFIG_PATH=projects/BEVFusion/configs/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
+VIEW_RECON_CONFIG_PATH=projects/BEVFusion/configs/view_recon.py
+
+# CKPT_PATH=projects/BEVFusion/checkpoints/bevfusion-trained.pth
+CKPT_PATH=projects/BEVFusion/checkpoints/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d-5239b1af.pth
+
 TRAINING_BATCH_SIZE=1
 TESTING_BATCH_SIZE=6
 
 cd $WORKSPACE_PATH
 export PYTHONPATH=$PYTHONPATH:$WORKSPACE_PATH
-if [ $MODE == "training" ]; then
+if [ $MODE == "pretraining" ]; then
+    python tools/train.py \
+        $LIDAR_ONLY_CONFIG_PATH \
+        --cfg-options \
+            train_dataloader.batch_size=$TRAINING_BATCH_SIZE
+elif [ $MODE == "training" ]; then
     python tools/train.py \
         $CONFIG_PATH \
         --cfg-options \
--- projects/BEVFusion/scripts/test.sh
+++ projects/BEVFusion/scripts/test.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 WORKSPACE_PATH=/workspace
-CONFIG_PATH=projects/BEVFusion/configs/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
-CKPT_PATH=/workspace/projects/BEVFusion/checkpoints/bevfusion-trained.pth
 
-BATCH_SIZE=3
+LIDAR_CAM_CONFIG_PATH=projects/BEVFusion/configs/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d.py
+VIEW_RECON_CONFIG_PATH=projects/BEVFusion/configs/view_recon.py
+
+CKPT_0_PATH=projects/BEVFusion/checkpoints/bevfusion-trained.pth
+CKPT_1_PATH=projects/BEVFusion/checkpoints/bevfusion_lidar-cam_voxel0075_second_secfpn_8xb4-cyclic-20e_nus-3d-5239b1af.pth
+CKPT_2_PATH=projects/BEVFusion/checkpoints/bevfusion-2.pth
+
+BATCH_SIZE=14
 
 cd $WORKSPACE_PATH
 export PYTHONPATH=$PYTHONPATH:$WORKSPACE_PATH
 
 # echo "1. No view drop testing"
 # python tools/test.py \
-#     $CONFIG_PATH \
-#     $CKPT_PATH \
+#     $VIEW_RECON_CONFIG_PATH \
+#     $CKPT_2_PATH \
 #     --cfg-options \
 #         test_dataloader.batch_size=$BATCH_SIZE \
 #         model.view_recon_cfg.enabled=False
@@ -20,8 +25,8 @@ export PYTHONPATH=$PYTHONPATH:$WORKSPACE_PATH
 
 # echo "2. View drop testing - without view reconstruction"
 # python tools/test.py \
-#     $CONFIG_PATH \
-#     $CKPT_PATH \
+#     $VIEW_RECON_CONFIG_PATH \
+#     $CKPT_2_PATH \
 #     --cfg-options \
 #         test_dataloader.batch_size=$BATCH_SIZE \
 #         model.view_recon_cfg.enabled=False
@@ -29,29 +34,42 @@ export PYTHONPATH=$PYTHONPATH:$WORKSPACE_PATH
 
 # echo "3. View drop testing - with view reconstruction"
 # python tools/test.py \
-#     $CONFIG_PATH \
-#     $CKPT_PATH \
+#     $VIEW_RECON_CONFIG_PATH \
+#     $CKPT_2_PATH \
 #     --cfg-options \
 #         test_dataloader.batch_size=$BATCH_SIZE
 # echo ===============================================
 
 # echo "4. All views dropped testing - without view reconstruction"
 # python tools/test.py \
-#     $CONFIG_PATH \
-#     $CKPT_PATH \
+#     $VIEW_RECON_CONFIG_PATH \
+#     $CKPT_2_PATH \
 #     --cfg-options \
 #         test_dataloader.batch_size=$BATCH_SIZE \
 #         model.view_recon_cfg.num_views_drop=6 \
 #         model.view_recon_cfg.enabled=False
 # echo ===============================================
 
-echo "5. Camera only testing - without view reconstruction"
-python tools/test.py \
-    $CONFIG_PATH \
-    $CKPT_PATH \
-    --cfg-options \
-        test_dataloader.batch_size=$BATCH_SIZE \
-        model.view_recon_cfg.num_views_drop=0 \
-        model.view_recon_cfg.enabled=False \
-        model.tmp_cfg.drop_pts_feature=True
-echo ===============================================
+# echo "5. Camera only testing - without view reconstruction"
+# python tools/test.py \
+#     $VIEW_RECON_CONFIG_PATH \
+#     $CKPT_2_PATH \
+#     --cfg-options \
+#         test_dataloader.batch_size=$BATCH_SIZE \
+#         model.view_recon_cfg.num_views_drop=0 \
+#         model.view_recon_cfg.enabled=False \
+#         model.tmp_cfg.drop_pts_feature=True
+# echo ===============================================
+
+
+for i in {1..5}
+do
+    python tools/test.py \
+        $VIEW_RECON_CONFIG_PATH \
+        $CKPT_2_PATH \
+        --cfg-options \
+            test_dataloader.batch_size=$BATCH_SIZE \
+            model.view_recon_cfg.num_views_drop=$i \
+            model.view_recon_cfg.enabled=False
+    echo ===============================================
+done
